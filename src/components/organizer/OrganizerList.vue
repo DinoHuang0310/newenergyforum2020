9336a90cb87668967b93103ea83a57ac9f33fd49
dino770310 <dino770310@gmail.com>
2020-09-07 10:23:18 +0800

    refactor: 排序方式更改

--- src/components/organizer/OrganizerList.vue
+++ src/components/organizer/OrganizerList.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="agencysList.list.length > 0">
         <h3>{{ agencysList.title }}</h3>
-        <ul :class="agencysList.list.length === 1 ? 'center' : null">
+        <ul :class="agencysList.list.length < 3 ? 'center' : null">
           <li
             v-for="list in agencysList.list"
             :key="list.index"
